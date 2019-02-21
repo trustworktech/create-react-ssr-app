db83a0706251356dbe767d534c84e6867f0eb492
Max Parelius <maxparelius@gmail.com>
2019-02-21 08:28:23 -0600

    Fixing html links

--- packages/react-ssr-scripts/template/src/server/middleware/render/HTML.js
+++ packages/react-ssr-scripts/template/src/server/middleware/render/HTML.js
@@ -4,13 +4,16 @@ const HTML = ({ scriptLinks, styleLinks, children }) => (
   <html lang="en">
     <head>
       <meta charSet="utf-8" />
-      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <link
+        rel="shortcut icon"
+        href={`${process.env.PUBLIC_URL}/favicon.ico`}
+      />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
       <meta name="theme-color" content="#000000" />
-      <link rel="manifest" href="/static/manifest.json" />
+      <link rel="manifest" href={`${process.env.PUBLIC_URL}/manifest.json`} />
       <title>React SSR App</title>
       {(scriptLinks || []).map(src => {
         return <script key={src} defer src={src} />;
