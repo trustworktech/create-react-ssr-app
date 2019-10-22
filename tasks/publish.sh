84f5eeffb338eefffd7f6e2b6fc3d2a356c1745d
Max Parelius <maxparelius@gmail.com>
2019-10-22 11:59:25 -0700

    Updating publish script

--- tasks/publish.sh
+++ tasks/publish.sh
@@ -26,21 +26,10 @@ set -x
 cd ..
 root_path=$PWD
 
-if [ -z $CI ]; then
-  yarn compile:lockfile
-fi
-
 if [ -n "$(git status --porcelain)" ]; then
   echo "Your git status is not clean. Aborting.";
   exit 1;
 fi
 
-# Get 2FA when not CI
-otp=""
-if [ -z $CI ]; then
-  echo "Please enter npm two-factor auth code: "
-  read otp
-fi
-
 # Go!
-NPM_CONFIG_OTP="$otp" ./node_modules/.bin/lerna publish --npm-client=npm "$@"
+./node_modules/.bin/lerna publish --npm-client=npm "$@"
